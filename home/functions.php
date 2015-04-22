2f7430fc0a42df38da7bb765138f96f04bcc13d7
dcm53f <dcm53f@mail.missouri.edu>
2015-04-21 22:11:54 -0500

    Update functions.php

--- home/functions.php
+++ home/functions.php
@@ -1,6 +1,49 @@
 <?php
-	if (isset($_POST['submitRegForm'])){
-		//print_r($_POST);
-		$pawprint = htmlspecialchars($_POST['pawprint']);
+	require '../secure/database.php';
+
+	if (isset($_POST['submitRegForm']))
+	{	
+		$username = htmlspecialchars($_POST['username']);
+		$user_type = htmlspecialchars($_POST['user-type']);
+		$institution_name = htmlspecialchars($_POST['institution_name']);
+		$firstname = htmlspecialchars($_POST['firstname']);
+		$lastname = htmlspecialchars($_POST['firstname']);
+		$email = htmlspecialchars($_POST['email']);
+		$address = htmlspecialchars($_POST['address']);
+		$city = htmlspecialchars($_POST['city']);
+		$state = htmlspecialchars($_POST['state']);
+		$zipcode = htmlspecialchars($_POST['zipcode']);
+		$username_exists = 0;
+		print_r($_POST);
+		exit();
+		try 
+		{
+			$conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+			// set the PDO error mode to exception
+			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+			// prepare sql and bind parameters
+			$stmt = $conn->prepare("SELECT username FROM User WHERE username LIKE :uname");
+			$stmt->execute(array(':uname'=> $username));
+			//print_r($_POST);
+			//exit();
+			$row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			
+			if (!empty($row))
+			{
+				echo "1";
+			}
+			else{
+				echo "0";
+			}
 		}
+		catch(PDOException $e)
+		{
+			echo "Error: " . $e->getMessage();
+		}
+		$conn = null;		
+
+	}
+		
+	
 ?>
